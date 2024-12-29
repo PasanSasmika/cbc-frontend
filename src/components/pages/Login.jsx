37b2e1056a65f2092d21b9bdb89adb36a3a9a4d2
PasanSasmika <pasansasmika198@gmail.com>
2024-12-29 06:34:48 +0530

    save the token to the localstorage👍

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,34 +1,59 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 function Login() {
+
+  const [email, setEmail] = useState("Your email")
+  const [password, setPassword] = useState("")
+
+  function userLogin(){
+
+    axios.post("http://localhost:5000/api/users/login",{
+      email : email,
+      password : password
+    }).then(
+      (res)=>{
+      
+
+        localStorage.setItem("token",res.data.token)
+
+
+      }
+    )
+  }
+
   return (
     <div class="flex items-center justify-center min-h-screen bg-gray-100">
     <div class="bg-white p-8 rounded-lg shadow-md w-96">
       <h2 class="text-2xl font-bold text-gray-700 text-center mb-6">Login</h2>
-      <form>
+      
         <div class="mb-4">
           <input 
             type="email" 
-            placeholder="Email" 
-            required 
+           defaultValue={email}
+           onChange={(e)=>{
+            setEmail(e.target.value)
+           }} 
             class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
         <div class="mb-4">
           <input 
             type="password" 
-            placeholder="Password" 
-            required 
+           defaultValue={password} 
+            onChange={(e)=>{
+              setPassword(e.target.value)
+            }}
             class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
         <button 
-          type="submit" 
+        onClick={userLogin}
           class="w-full bg-blue-500 text-white py-2  rounded-lg hover:bg-blue-600 transition duration-200"
         >
           Login
         </button>
-      </form>
+
     </div>
   </div>
   
