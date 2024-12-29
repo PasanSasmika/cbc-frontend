e9706ef0ab6df7847071d075d5441b13e0807a04
PasanSasmika <pasansasmika198@gmail.com>
2024-12-29 06:37:28 +0530

    user validation created👍👁️

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -13,10 +13,18 @@ function Login() {
       password : password
     }).then(
       (res)=>{
-      
+       
+        if(res.data.user== null){
+          return
+        }
 
         localStorage.setItem("token",res.data.token)
-
+        
+        if(res.data.user.type == "admin"){
+          window.location.href = "/admin"
+        }else{
+          window.location.href = "/"
+        }
 
       }
     )
