e72d4b39f974f192c799f619e949c8de98155fe1
PasanSasmika <pasansasmika198@gmail.com>
2025-01-10 08:59:36 +0530

    add some framer animations 🎉

---
+++ src/animations/animation.js
@@ -0,0 +1,26 @@
+export const Animation = () => ({
+    whileTap: {
+      scale: 2,
+      rotate: 180,
+    },
+    transition: {
+      duration: 0.7,
+    },
+  });
+
+
+  export const Animation2 = () => ({
+
+    initial:{
+        x:-60
+      },
+      
+      animate:{
+        x: -7
+      
+      },
+      transition:{
+        duration:0.8
+      }
+      
+  });
