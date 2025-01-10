1f85c03fd0fee261b119696e7c202b65831ff2d0
PasanSasmika <pasansasmika198@gmail.com>
2025-01-10 08:33:22 +0530

    add fonts tailwind config file 🔠👍

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        main: ["Playfair Display"], 
+        second: ["Bonheur Royale"],
+        third: ["Alumni Sans"] 
+      }
+    },
   },
   plugins: [],
 }
