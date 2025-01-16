093227fbeda806320924d326bf54635cfb2622f0
PasanSasmika <pasansasmika198@gmail.com>
2025-01-16 18:14:29 +0530

    Create framer animation for the page 🪽💢

--- src/animations/animation.js
+++ src/animations/animation.js
@@ -30,7 +30,7 @@ export const HomePageAnimation = () => ({
     scale: 1,
   },
   transition: {
-    duration: 1.3,
+    duration: 1,
   },
   
 });
@@ -43,6 +43,45 @@ export const HomePageAnimation2 = () => ({
 
   whileInView: { opacity: 1, scale: 1 },
   
-  transition: { duration: 1.3 },
+  transition: { duration: 1 },
   
 });
+
+
+export const SkinImprove1 = () => ({
+
+  initial: { x: -200},
+
+  whileInView: { x: 1 } ,
+    
+  transition: { duration: 1.3 },
+
+    
+});
+
+export const SkinImprove2 = () => ({
+
+  
+  initial: { opacity: 0, scale: 0 },
+
+  whileInView: { opacity: 1, scale: 1 },
+  
+  transition: { duration: 1.1 },
+
+    
+});
+
+
+      export const SkinImprove3 = () => ({
+
+  
+        animate:{ rotate: 360 },
+        transition:{
+        duration: 7, 
+        repeat: Infinity,
+        ease: "linear", 
+      }
+
+      
+          
+      });
--- src/pages/homepages/SkinImprove.jsx
+++ src/pages/homepages/SkinImprove.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import image1 from "../../../public/image1.jpg";
 import image2 from "../../../public/image2.jpg";
 import image3 from "../../../public/image32.png";
+import { motion } from 'framer-motion' 
+import { HomePageAnimation2, SkinImprove1, SkinImprove3 } from "../../animations/animation";
 
 
 function SkinImprove() {
   return (
     <div className="h-screen w-full flex bg-yellow-300">
       <div className="flex bg-primary h-full w-[50%] relative">
-        <div className="flex items-center justify-center">
+        <motion.div {...SkinImprove1()} className="flex items-center justify-center">
           <img
             src={image2}
             alt=""
@@ -21,20 +23,20 @@ function SkinImprove() {
               className="w-[360px] h-[400px] object-cover"
             />
             <div className=" absolute bottom-0 right-0 -z-10">
-                <img src={image3} alt="" className="w-32 h-32 relative top-14 left-20" />
+                <motion.img {...SkinImprove3()} src={image3} alt="" className="w-32 h-32 relative top-14 left-20" />
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
       <div className="flex items-center justify-center bg-primary h-full w-[50%]">
         <div className=" absolute">
-            <h1 className="text-[47px] text-black font-medium font-main relative bottom-52">Improve your skin’s <br></br>well-begin with our </h1>
+            <motion.h1 {...HomePageAnimation2()} className="text-[47px] text-black font-medium font-main relative bottom-52">Improve your skin’s <br></br>well-begin with our </motion.h1>
         </div>
         <div className=" absolute">
-        <h1 className="text-[80px] text-secondary font-medium font-second relative bottom-16">Skincare</h1>
+        <motion.h1 {...HomePageAnimation2()} className="text-[80px] text-secondary font-medium font-second relative bottom-16">Skincare</motion.h1>
         </div>
         <div className='absolute'>
-            <h1 className='text-[26px] font-accent text-secondary relative top-3 '>Shop here</h1>
+            <motion.h1 {...HomePageAnimation2()} transition={{duration: 1.2}} className='text-[26px] font-accent text-secondary relative top-3 '>Shop here</motion.h1>
           </div>
           
       </div>
