5b81334a9ecc9031de83ad6311002f0b64d58bc2
PasanSasmika <pasansasmika198@gmail.com>
2025-01-21 08:21:53 +0530

    remove public folder in gitignore😪

--- src/pages/homepages/Home.jsx
+++ src/pages/homepages/Home.jsx
@@ -14,8 +14,8 @@ function Home() {
     },[])
   return (
     <>
-    <div className='h-[110vh] flex'>
-      <div className='w-[35%] h-full bg-primary flex items-center'>
+    <div className='h-[130vh] flex'>
+      <div className='w-[35%] h-full bg-primary pb-64 flex items-center'>
         <motion.div {...HomePageAnimation2()}  className='relative bottom-14 left-32 z-20'>
           <h1 className='text-[85px] text-black font-medium font-main'>Discover the beauty within</h1>
           <div className='absolute'>
@@ -26,8 +26,8 @@ function Home() {
           </div>
         </motion.div>
       </div>
-      <div className='w-[65%] bg-primary flex items-center z-10'>      
-        <motion.img src={home} alt="" {...HomePageAnimation()} className='w-[1040px] h-[573px] rounded-md shadow-lg object-cover right-9 relative top-3' />
+      <div className='w-[65%] bg-primary flex items-center pb-64 z-10'>      
+        <motion.img src={home} alt="" {...HomePageAnimation()} className='w-[1040px] h-[573px]  rounded-md shadow-lg object-cover right-9 relative top-3' />
       </div>
       
     </div>
