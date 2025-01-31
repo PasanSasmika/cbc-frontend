325bbfcef251ed0796215a9af06f7b74466bb269
PasanSasmika <pasansasmika198@gmail.com>
2025-01-31 17:58:17 +0530

    category page

--- src/pages/homepages/Category.jsx
+++ src/pages/homepages/Category.jsx
@@ -2,6 +2,9 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import SecondaryLoader from '../../components/SecondaryLoader';
+import Header from '../../components/Header';
+import image2 from "/faqH.jpg";
+import { motion } from "framer-motion";
 
 function Category() {
     const { category } = useParams();
@@ -23,9 +26,34 @@ function Category() {
 
   return (
     <>
-{ loading? <SecondaryLoader/> :<div>
-<h1 className="text-2xl font-semibold text-gray-800 mb-6">{category} Products</h1>
-<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+    <Header/>
+{ loading? <SecondaryLoader/> :
+
+<div className='w-full h-[100vh] bg-primary'>
+<div className="w-full h-[370px] bg-primary flex items-center justify-center mt-8 relative">
+        <div className="w-[1404px] h-full relative">
+          <img
+            src={image2}
+            alt=""
+            className="w-full h-full object-cover rounded-xl"
+          />
+          <div className="absolute top-0 left-0 w-full h-full bg-black/35 rounded-xl z-10 flex flex-col items-center justify-center">
+            <motion.h1
+            
+              className="text-white font-main font-bold text-[80px] z-40"
+            >
+              {category} 
+            </motion.h1>
+            <motion.h1
+         
+              className="text-white font-second text-[65px] z-40"
+            >
+             products
+            </motion.h1>
+          </div>
+        </div>
+      </div>
+<div className="bg-primary w-full h-full flex flex-wrap overflow-y-scroll gap-16 mt-44 justify-center">
 {products.map((product) => (
     <div
     key={product._id}
