119690700f9dc2a6eed36010eb779f6b66670aad
PasanSasmika <pasansasmika198@gmail.com>
2025-02-02 10:24:26 +0530

    testing file deleted

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import AdminHome from './pages/adminPages/AdminHome'
 import { Toaster } from 'react-hot-toast'
 import SignUp from './pages/homepages/SignUp'
 import Login from './pages/homepages/Login'
-import Testing from './pages/adminPages/Testing'
 import CustomerHome from './pages/homepages/CustomerHome'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
@@ -23,7 +22,6 @@ function App() {
      <Route path='/' element={<CustomerHome/>}/>
      <Route path='/login' element={<Login/>}/>
      <Route path='/signup' element={<SignUp/>}/>
-     <Route path='/test' element={<Testing/>}/>
      <Route path='/admin/*' element={<AdminHome/>}/>
      <Route path='/*' element={<CustomerHome/>}/>
 
